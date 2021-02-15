6dc6be795d4a3e94daeb8f0f25ffbb5a6bb36b12
Andreas Manessinger <andreas.manessinger@wien.gv.at>
2021-02-15 08:35:31 +0100

    Rename backup script Backup Music

--- bin/backup_macbook.sh
+++ bin/backup_macbook.sh
@@ -6,3 +6,10 @@ mkdir -p /Volumes/Photos/Lightroom/
 for d in 20* ; do
   rsync -rv --size-only --delete "$d" "/Volumes/Photos/Lightroom/$d"
 done
+
+cd ~/Music || exit 1
+mkdir -p /Volumes/Photos/Music/
+
+for d in Classic Misc other ; do
+  rsync -rv --size-only --delete "$d" "/Volumes/Photos/Music/$d"
+done
